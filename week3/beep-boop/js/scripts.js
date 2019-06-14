edfa032926d09dee7e3a1d89a59f5b8c2acec65c
Ben Bennett-Cauchon <bbcauchon@gmail.com>
2019-06-14 10:55:54 -0700

    add User Interface logic, basic functionality and styles

--- week3/beep-boop/js/scripts.js
+++ week3/beep-boop/js/scripts.js
@@ -1,11 +1,39 @@
+// -- User Interface -----------------------------------------------------------
 $(document).ready(function() {
-  $("#some-form").submit(function(event) {
+  $("#form").submit(function( event ) {
     event.preventDefault();
 
-    var someInputVar = $("#some-input").val(); // Don't forget it's a string without parseInt()
-    var someOutputVar = someInputVar;
+    var inputNumber = parseInt($("#numberInput").val());
 
-    $("#output").text(someOutputVar);
-    $("#output").show();
+    $("#outputList").empty();
+    if (inputNumber && inputNumber >= 0) {
+      $("#beepBoopNumber").text(inputNumber);
+      beepBoopList(inputNumber).forEach(function(item) {
+        $("#outputList").append(newListItem(item));
+      });
+      $("#output").show();
+      $("#form")[0].reset();
+    } else {
+      // TODO
+    }
   });
 });
+
+function newListItem(text) {
+  return "<li>" + text + "</li>";
+}
+
+// -- Business Logic -----------------------------------------------------------
+
+// Returns an array of numbers from 0 up to the given number, with the following
+// exceptions:
+// * Numbers that contain a 1: all digits are replaced (all digits) with "Beep!"
+// * Numbers that contain a 2: all digits are replaced (all digits) with "Boop!"
+// * Numbers that contain a 3: all digits are replaced (all digits) with "I'm sorry, Dave. I'm afraid I can't do that."
+function beepBoopList(number) {
+  var list = [];
+  for (var i = 0; i <= number; i++) {
+    list[i] = i;
+  }
+  return list;
+}
